b8403b05cf388c7fcb4127ecb83222cdc47cb12c
Matthew Emerson <emersonmde@protonmail.com>
2019-08-29 23:07:57 -0400

    Setting AFK on character selection

--- classic_queue.py
+++ classic_queue.py
@@ -3,13 +3,25 @@
 import pyautogui
 import time
 import random
+import os
+import sys
+
+def resourcePath(relativePath):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        basePath = sys._MEIPASS
+    except Exception:
+        basePath = os.path.abspath(".")
+
+    return os.path.join(basePath, relativePath)
 
 def findLocation():
     pass
     screen = pyautogui.screenshot()
-    realmSelect = pyautogui.locate('images/realm_selection.png', screen, grayscale=True, confidence=.9)
-    changeRealm = pyautogui.locate('images/change_realm.png', screen, grayscale=True, confidence=.9)
-    enterWorld = pyautogui.locate('images/enter_world.png', screen, grayscale=True, confidence=.9)
+    realmSelect = pyautogui.locate(resourcePath('images/realm_selection.png'), screen, grayscale=True, confidence=.9)
+    changeRealm = pyautogui.locate(resourcePath('images/change_realm.png'), screen, grayscale=True, confidence=.9)
+    enterWorld = pyautogui.locate(resourcePath('images/enter_world.png'), screen, grayscale=True, confidence=.9)
 
     if realmSelect is not None:
         return 'REALM_SELECT'
@@ -20,11 +32,16 @@ def findLocation():
     else:
         return 'IN_GAME'
 
+# Give enough time to open wow
+time.sleep(30)
 while True:
     location = findLocation()
 
     if location == 'REALM_SELECT':
-        server = pyautogui.locateOnScreen('server.png', grayscale=True, confidence=.9)
+        # We're on the realm selection screen
+        # Click the correct server and then click okay
+        print('Selecting realm...')
+        server = pyautogui.locateOnScreen(resourcePath('server.png'), grayscale=True, confidence=.9)
         if server is None:
             time.sleep(2)
             continue
@@ -33,7 +50,7 @@ while True:
         pyautogui.moveTo(serverPoint.x, serverPoint.y, .75)
         pyautogui.click()
 
-        realmSelectOkay = pyautogui.locateOnScreen('images/realm_selection_okay.png', grayscale=True, confidence=.9)
+        realmSelectOkay = pyautogui.locateOnScreen(resourcePath('images/realm_selection_okay.png'), grayscale=True, confidence=.9)
         if realmSelectOkay is None:
             time.sleep(2)
             continue
@@ -42,31 +59,42 @@ while True:
         pyautogui.moveTo(realmSelectOkayPoint.x, realmSelectOkayPoint.y, .75)
         pyautogui.click()
 
-        #time.sleep(10)
-        time.sleep(5)
-
+        time.sleep(10)
     elif location == 'CHANGE_REALM':
-        #time.sleep(5)
-        time.sleep(60)
+        # Welcome to the queue.. /afk
+        print('Waiting in queue - moving mouse')
+        windowSize = pyautogui.size()
+        x1 = windowSize.width / 2
+        y1 = windowSize.height / 2
+        x1 = x1 - random.randrange(2, 50)
+        y1 = y1 - random.randrange(2, 50)
+
+        x2 = x1 + random.randrange(2, 50)
+        y2 = y1 + random.randrange(2, 50)
+
+        pyautogui.moveTo(x1, y1, 1)
+        pyautogui.moveTo(x2, y2, 1)
+        time.sleep(30)
     elif location == 'ENTER_REALM':
-        #time.sleep(30)
-        time.sleep(5)
-        enterRealm = pyautogui.locateOnScreen('images/enter_world.png', grayscale=True, confidence=.9)
-        if enterRealm is None:
-            time.sleep(2)
-            continue
+        print('Submitting inputs on the character selection screen')
+        # Made it to character selection
+        # Randomly hit left/right and add some mouse drags
 
-        enterRealmPoint = pyautogui.center(enterRealm)
-        pyautogui.moveTo(enterRealmPoint.x, enterRealmPoint.y, .75)
-        pyautogui.click()
+        # time.sleep(5)
+        # enterRealm = pyautogui.locateOnScreen('images/enter_world.png', grayscale=True, confidence=.9)
+        # if enterRealm is None:
+        #     time.sleep(2)
+        #     continue
 
-    else:
-        keys = ['left', 'right', 'space']
+        # enterRealmPoint = pyautogui.center(enterRealm)
+        # pyautogui.moveTo(enterRealmPoint.x, enterRealmPoint.y, .75)
+        # pyautogui.click()
+
+        keys = ['left', 'right']
         keyChoice = random.choice(keys)
         pyautogui.press(keyChoice)
 
-        #time.sleep(random.randrange(5, 30))
-        time.sleep(random.randrange(1, 3))
+        time.sleep(random.randrange(1, 30))
 
         # Add some mouse drags for good measure
         windowSize = pyautogui.size()
@@ -79,8 +107,13 @@ while True:
         y2 = y1 + random.randrange(2, 50)
 
         pyautogui.moveTo(x1, y1, 1)
-        pyautogui.dragTo(x2, y2, duration=random.randrange(1, 3), button='right')
+        pyautogui.dragTo(x2, y2, duration=random.randrange(1, 3), button='left')
 
-        #time.sleep(5)
+        time.sleep(random.randrange(1, 30))
+    else:
+        # Not sure where we are, wait and hope we end up somehwere better
+        print('Not in game')
+        time.sleep(30)
+        pass
 
 
